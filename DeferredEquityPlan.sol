d3218edbd3fb0be893b6298ec0f6119bb3d32175
xfdobs <68253886+xfdobs@users.noreply.github.com>
2021-08-12 15:27:58 -0400

    Fixing the code - final version

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -1,42 +1,66 @@
-1) Com dados de consumo (CNPJ/Nome do local e valores)
-   - Atraves de API's, conseguir identificar o local consumido
-   - Mensurar a frequencia de consumo
-   - Desenvolver parcerias com os lugares de maiores paradas/consumos
+pragma solidity ^0.5.0;
 
-* Usar os dados de consumo atuais, treinar modelos para prever consumo
+// lvl 3: equity plan
+contract DeferredEquityPlan {
+    address human_resources;
 
-1-1 : taxas : 1.5 2.4 ate 3.5
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    //Set the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribuition = 250;
+
+    uint start_time = now; // permanently store the time this contract was initialized
+
+    //Set the `unlock_time` to be 365 days from now
+    uint public unlock_time = start_time + 365 days;
+    uint public fakenow = now;
+
+    uint public distributed_shares;
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+
+        // Add "require" statements to enforce that:
+        // 1: `unlock_time` is less than or equal to `now`
+        require(unlock_time <= fakenow,"Shares are not vested yet");
+        // 2: `distributed_shares` is less than the `total_shares`
+        require(distributed_shares < total_shares,  "All the shares have been already distributed");
+
+        //Add 365 days to the `unlock_time`
+        unlock_time = fakenow + 365 days;
+
+        // Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
+        distributed_shares = ((fakenow - start_time) / 365) * annual_distribuition;
 
 
+        // double check in case the employee does not cash out until after 5+ years
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
 
+    // human_resources and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
 
+    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
+    
+    function fastforward() public {
+    fakenow += 100 days;
+}
 
-
-Ultimo Sorteio - 150k
-Qual foi o valor gerado nessa campanha ?
-net foi positivo ?
-2% gasto sobre o valor do prete, total do frete 
-identificar gasto medio dos motoristas
-
-40% do valor do frete, vira disivel 
-
-ftaxa de antecipacao
-3 dias 100 reais, 
-300k 1 mes = 1% - 5%
-
-
-
-
-
-2) Rewards com base no nivel de consumo
-   - Ate um nivel X, descontar x % da taxa de adiantamento
-   - Ate um nivel X, antecipar em um periodo mais curto
-     - Descobrir o valor medio de gasto de um caminhoneiro e atacar esse valor
-     - Quanto tempo demora para um caminhoneiro receber
-	 
-     - Confirmar valor de taxa 
-	 - Qual impacto na antecipacao
-	 
-	 
-	 
-	 
+}
